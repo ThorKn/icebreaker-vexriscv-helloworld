5d3477dceffe3fb6d8f7518f70d2315823b66522
Thorsten Knoll <thorsten_knoll@yahoo.de>
2020-09-16 18:17:37 +0200

    update hello_world

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-all: bitstream
+all: bitstream hello_world
 
 bitstream:
 	cd vexriscv/rtl ; $(MAKE)
 
+hello_world:
+	cd hello_world ; $(MAKE)
+
 clean:
 	cd vexriscv/rtl ; $(MAKE) clean
+	cd hello_world ; $(MAKE) clean
---
+++ hello_world/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <murax.h>
+
+int main() {
+	printf("Hello World\n");
+	while (1) {
+		printf("Hello Steffen\n");
+	}
+	return 0;
+}
+
+void irqCallback(){
+}
+
